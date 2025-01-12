a02aa9c07c1f05d1d3e8ce91d585adbf616e7d70
shiropt <63380875+shiropt@users.noreply.github.com>
2025-01-12 13:33:03 +0900

    add authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { z } from "zod";
-
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { db } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -105,4 +106,23 @@ export async function deleteInvoice(id: string) {
   }
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
